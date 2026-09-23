2d8a7b4223def01fb972f4e2d011f2267816be5a
Schrever <jbb090410@gmail.com>
2026-09-23 09:42:04 +0200

    commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 2f;
+    public int health = 1;
+    public Transform[] waypoints;
+
+    public int currentWayPoint = 0;
+    void Update()
+    {
+        if(waypoints == null || waypoints.Length == 0) return;
+
+        Transform target = waypoints[currentWayPoint];
+        Vector3 dir = (target.position - transform.position).normalized;
+
+        transform.position += dir * speed * Time.deltaTime;
+
+        if(Vector3.Distance(transform.position, target. position) < 0.05f)
+        {
+            currentWayPoint++;
+
+            if(currentWayPoint >= waypoints.Length)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}

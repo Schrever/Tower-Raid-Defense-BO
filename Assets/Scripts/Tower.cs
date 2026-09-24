51cde59be39a33682516798a0c18eeae8160155c
Schrever <jbb090410@gmail.com>
2026-09-24 13:08:22 +0200

    upgrades1

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,5 +1,16 @@
 using UnityEngine;
 
+[System.Serializable]
+public class TowerUpgradeStage
+{
+    public float range;
+    public float fireRate;
+    public Sprite sprite;
+    public int price;
+    public GameObject projectilePrefab;
+
+}
+
 public class Tower : MonoBehaviour
 {
     public float range = 3f;
@@ -7,9 +18,21 @@ public class Tower : MonoBehaviour
     public GameObject projectilePrefab;
     public Transform firePoint;
 
+    public TowerUpgradeStage[] upgradeStages;
+    public int upgradeStage = 0;
+    private SpriteRenderer sr;
+    public GameObject towerUpgradeUIPrefab;
+    private GameObject currentUI;
+
+
     public int towerPrice = 1;
 
     private float fireCooldown = 0f;
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
@@ -53,5 +76,33 @@ public class Tower : MonoBehaviour
         GameObject p = Instantiate(projectilePrefab,firePoint.position, Quaternion.identity);
         Projectile pr = p.GetComponent<Projectile>();
         pr.target = target.transform;
+    }
+
+    public void Upgrade()
+    {
+        TowerUpgradeStage currentUpgradeStage = upgradeStages[upgradeStage];
+
+        range = currentUpgradeStage.range;
+        fireRate = currentUpgradeStage.fireRate;
+        sr.sprite = currentUpgradeStage.sprite;
+        projectilePrefab = currentUpgradeStage.projectilePrefab;
+        CashManager.instance.UpdateCoins(-currentUpgradeStage.price);
+        upgradeStage += 1;
+    }
+
+    private void OnMouseDown()
+    {
+        if(currentUI == null)
+        {
+            currentUI = Instantiate(towerUpgradeUIPrefab, FindObjectOfType<Canvas>().transform);
+        }
+
+        TowerUpgradeUI currentUpgradeUI = currentUI.GetComponent<TowerUpgradeUI>();
+        currentUpgradeUI.tower = this;
+
+        currentUI.transform.position = Input.mousePosition + new Vector3(50, -50);
+
+        if (upgradeStage >= upgradeStages.Length) return;
+        currentUpgradeUI.priceTxt.text = upgradeStages[upgradeStage].price.ToString();
     }
 }
